c7214347f6eb2c66b3e0db50b49ecb4ad6c7a69d
HAO Ngo Van (VTI.D8) <hao.ngovan@vti.com.vn>
2023-06-26 10:16:38 +0700

    btvn2

--- BtvnBuoi2/Bai1.java
+++ BtvnBuoi2/Bai1.java
@@ -1,2 +1,24 @@
-package BtvnBuoi2;public class Bai1 {
+package BtvnBuoi2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+  public static void main(String[] args) {
+    int a, b, c;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhập hệ số a: ");
+    a = scanner.nextInt();
+    System.out.println("Nhập hệ số b: ");
+    b = scanner.nextInt();
+    System.out.println("Nhập hệ số c: ");
+    c = scanner.nextInt();
+    System.out.println("Số lớn nhất trong 3 số là: ");
+    if (a >= b && a >= c) {
+      System.out.println(a);
+    } else if (b >= c) {
+      System.out.println(b);
+    } else {
+      System.out.println(c);
+    }
+  }
 }
---
+++ BtvnBuoi2/Bai4.java
@@ -0,0 +1,30 @@
+package BtvnBuoi2;
+
+import java.util.Scanner;
+
+public class Bai4 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Nhập vào chuỗi cần chuẩn hóa: ");
+    String input = scanner.nextLine();
+    String result = normalizeString(input);
+    System.out.println("Chuỗi sau khi chuẩn hóa: " + result);
+  }
+
+  public static String normalizeString(String input) {
+    // Xóa khoảng trắng thừa 2 đầu
+    input = input.trim();
+    // Thay thế các khoảng trắng liên tiếp bằng 1 khoảng trắng duy nhất
+    input = input.replaceAll("\\s+", " ");
+    // Kiểm tra và loại bỏ các ký tự số
+    input = input.replaceAll("\\d", "");
+    String[] words = input.split(" ");
+    StringBuilder result = new StringBuilder();
+    for (String word : words) {
+      // Viết hoa ký tự đầu của từ
+      String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+      result.append(capitalized).append(" ");
+    }
+    return result.toString().trim();
+  }
+}
